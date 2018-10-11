1f594f376e87fa8cf850dd366e45fa7815a1abfa
Abhishek Biswas <abhishek.blogger05@gmail.com>
2018-10-11 09:09:17 +0530

    fixed BGchangestop but start running on pageload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-  var lolClock = document.getElementById('lol-clock');
-  var imgTxt = document.getElementById('img-txt');
-  var catImg = document.getElementById('cat-img');
-  var clockBody = document.getElementById('clock-body');
-  var partyBtn = document.getElementById('partyBtn');
+  var lolClock = document.getElementById("lol-clock");
+  var imgTxt = document.getElementById("img-txt");
+  var catImg = document.getElementById("cat-img");
+  var clockBody = document.getElementById("clock-body");
+  var partyBtn = document.getElementById("partyBtn");
   var partyTime = false;
-  var meow = new Audio('media/party.mp3');
+  var catMusic = new Audio("media/party.mp3");
+  var i = 0;
+
+
 //  var bodyHTML = document.getElementsByTagName("body")[0];
 
 // play the cat sound
-//var meow = new Audio('media/meow.mp3');
-//meow.play();
+//var catMusic = new Audio('media/catMusic.mp3');
+//catMusic.play();
 
 function timeNow() {
 
@@ -21,14 +24,14 @@ function timeNow() {
   var hour = day.getHours();
   var min = day.getMinutes();
   var sec = day.getSeconds();
-  var meridian = 'AM';
+  var meridian = "AM";
 
 // adding 0 before mind and secs
   if (min < 10) {
-    min = '0'+min;
+    min = "0"+min;
   }
   if (sec < 10) {
-    sec = '0'+sec;
+    sec = "0"+sec;
   }
 
 // sample test
@@ -57,59 +60,59 @@ if (hour > 5 && hour < 12) {
   // doing 5 <= hour < 12 didn't work
   switch (true) {
     case hour <= 5:
-      imgTxt.innerHTML = 'GET SOME SLEEPZ BRO';
+      imgTxt.innerHTML = "GET SOME SLEEPZ BRO";
       catImg.style.background = "url('img/earlyMorning.jpg')";
-      catImg.style.backgroundSize = 'cover';
-      document.body.style.backgroundColor= '#2d3037';
+      catImg.style.backgroundSize = "cover";
+      document.body.style.backgroundColor= "#2d3037";
       clockBody.style.color = "#99ffcc";
       break;
     case hour >= 5 && hour < 12:
-      imgTxt.innerHTML = 'GOOD MORNING';
+      imgTxt.innerHTML = "GOOD MORNING";
       catImg.style.background = "url('img/morning.jpg')";
-      catImg.style.backgroundSize = 'cover';
-      document.body.style.backgroundColor= '#fee0be';
+      catImg.style.backgroundSize = "cover";
+      document.body.style.backgroundColor= "#fffb8c";
       clockBody.style.color = "#40232e";
       break;
-    case hour == 12:
-      imgTxt.innerHTML = 'GOOD NOON';
+    case hour === 12:
+      imgTxt.innerHTML = "GOOD NOON";
       catImg.style.background = "url('img/noon.jpg')";
-      catImg.style.backgroundSize = 'cover';
-      document.body.style.backgroundColor= '#f2d16d';
+      catImg.style.backgroundSize = "cover";
+      document.body.style.backgroundColor= "#f2d16d";
       clockBody.style.color = "#400101";
-      meridian = 'PM';
+      meridian = "PM";
       break;
     case hour > 12 && hour <18:
-      imgTxt.innerHTML = 'GOOD AFTERNOON';
+      imgTxt.innerHTML = "GOOD AFTERNOON";
       catImg.style.background = "url('img/afternoon.jpg')";
-      catImg.style.backgroundSize = 'cover';
-      document.body.style.backgroundColor= '#f2a950';
+      catImg.style.backgroundSize = "cover";
+      document.body.style.backgroundColor= "#f2a950";
       clockBody.style.color = "#400101";
-      meridian = 'PM';
+      meridian = "PM";
       break;
     case hour >= 18 && hour <22:
-      imgTxt.innerHTML = 'GOOD EVENING';
+      imgTxt.innerHTML = "GOOD EVENING";
       catImg.style.background = "url('img/evening.jpg')";
-      catImg.style.backgroundSize = 'cover';
-      document.body.style.backgroundColor= '#425b8c';
+      catImg.style.backgroundSize = "cover";
+      document.body.style.backgroundColor= "#425b8c";
       clockBody.style.color = "#81c4f8";
       clockBody.style.textShadow = "5px 5px 15px #271c2c";
-      meridian = 'PM';
+      meridian = "PM";
       break;
     case hour >= 22 && hour <= 23:
-      imgTxt.innerHTML = 'GOOD NIGHT';
+      imgTxt.innerHTML = "GOOD NIGHT";
       catImg.style.background = "url('img/night.jpg')";
-      document.body.style.backgroundColor= '#271c2c';
+      document.body.style.backgroundColor= "#271c2c";
       clockBody.style.color = "#81c4f8";
-      meridian = 'PM';
+      meridian = "PM";
       break;
     default:
 
   }
 
-  lolClock.innerHTML = hour +':'+ min+':' + sec + ' ' + meridian;
+  lolClock.innerHTML = hour +":"+ min+":" + sec + " " + meridian;
 }
 
-setInterval(timeNow, 1000);
+var showTimeNow = setInterval(timeNow, 1000);
 
 
 
@@ -118,17 +121,17 @@ var playing = false;
 
 /*
 function partyTime() {
-  var meow = new Audio('media/party.mp3');
+  var catMusic = new Audio('media/party.mp3');
 
   if (playing === false) {
     playing = true;
     console.log("playing");
-    meow.play();
+    catMusic.play();
     partyBtn.innerHTML = "PARTY OVER";
   } else {
     playing = false;
-    meow.pause();
-    meow.currentTime = 0;
+    catMusic.pause();
+    catMusic.currentTime = 0;
     partyBtn.innerHTML = "PARTY TIME";
   }
 }
@@ -136,29 +139,38 @@ function partyTime() {
 
 
 
-partyBtn.addEventListener('click', partyEvent);
+partyBtn.addEventListener("click", partyEvent);
+partyBtn.addEventListener("click", changeBG);
+
+
 
 function partyEvent() {
-// partyTime and meow are defined outside
+// partyTime and catMusic are defined outside
 // otherwise they get re-defined without changing the old definition
   if (partyTime === false) {
     partyTime = true;
-    meow.play();
-    setInterval(changeBG, 1000);
+    catMusic.play();
     partyBtn.innerHTML = "PARTY OVER";
   } else {
     partyTime = false;
-    meow.pause();
-    clearInterval(changeBG, 1000);
+    catMusic.pause();
+    endBGchange();
     partyBtn.innerHTML = "PARTY TIME";
   }
 
-
 }
 
-var i = 0;
+
+
 function changeBG() {
-    var color = ["black", "blue", "brown", "green"];
+  document.body.style.backgroundColor = "";
+  var color = ["cyan", "blue", "brown", "green"];
     document.body.style.backgroundColor = color[i];
     i = (i + 1) % color.length;
+
+}
+var initBG = setInterval(changeBG, 1000);
+
+function endBGchange() {
+  clearInterval(initBG);
 }
